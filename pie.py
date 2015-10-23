b3efdf2879bb562c7211a8ec4f763868618fd446
mansoorsiddiqui <mis43@drexel.edu>
2015-10-23 13:54:40 -0400

    oct 23

--- pie.py
+++ pie.py
@@ -1,21 +1,22 @@
 from pylab import *
 
-# make a square figure and axes
-figure(1, figsize=(6,6))
-ax = axes([0.1, 0.1, 0.8, 0.8])
+totalStudents = {'BACS': 50, 'BSCS': 38, 'BASE': 5, 'BSSE': 10}
+metStudents = {'BACS': 48, 'BSCS': 33, 'BASE': 3, 'BSSE': 8}
 
-# The slices will be ordered and plotted counter-clockwise.
-labels = 'Met threshold', 'Did not meet threshold'
-fracs = [17.75, 82.25]
-explode=(0.1, 0)
+def createPieCharts(grade, total, met, minThreshold, maxThreshold):
+    for major, total in total.items():
+        # make a square figure and axes
+        figure(1, figsize=(6,6))
+        ax = axes([0.1, 0.1, 0.8, 0.8])
+        # The slices will be ordered and plotted counter-clockwise.
+        labels = 'Met threshold', 'Did not meet threshold'
+        fracs = [met[major]/total, (total-met[major])/total]
+        explode=(0.1, 0)
+        pie(fracs, explode=explode, labels=labels,
+               autopct='%1.1f%%', shadow=True, startangle=90)
+        title('%s Students Between %s-%s for Assignment %s' % (major, minThreshold, maxThreshold, grade),
+                 bbox={'facecolor':'0.8', 'pad':5})
+        savefig("piechart_%s_%s_%s-%s.png" % (grade, major, minThreshold, maxThreshold), bbox_inches="tight")
+        clf()
 
-pie(fracs, explode=explode, labels=labels,
-                autopct='%1.1f%%', shadow=True, startangle=90)
-                # The default startangle is 0, which would start
-                # the Frogs slice on the x-axis.  With startangle=90,
-                # everything is rotated counter-clockwise by 90 degrees,
-                # so the plotting starts on the positive y-axis.
-
-title('BACS Students between 50-75% for A3 Grade', bbox={'facecolor':'0.8', 'pad':5})
-
-savefig("piechart_A3_BACS_50-75.png", bbox_inches="tight")
+createPieCharts("A2", totalStudents, metStudents, 80, 100)
