796357036c9ca5f16b8b1154a4e99a227be803fb
Amndeep Singh Mann <amndeep.dev@gmail.com>
2015-10-29 13:52:03 -0400

    Completed analysis portion
    Checks to see how many students per major are within the threshold
    inclusive on the low end, exclusive on the high end.

--- range.py
+++ range.py
@@ -5,13 +5,13 @@ def get_arguments():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('gradebook')
 	parser.add_argument('majorlist')
-	parser.add_argument('range')
+	parser.add_argument('range_results')
 	parser.add_argument('grade_id')
 	parser.add_argument('grade_column')
 	parser.add_argument('major_id')
 	parser.add_argument('major_column')
-	parser.add_argument('lower_threshold', type=int)
-	parser.add_argument('higher_threshold', type=int)
+	parser.add_argument('lower_threshold', type=float)
+	parser.add_argument('higher_threshold', type=float)
 	return parser.parse_args()
 
 # usage: python range.py "filepathtogradebook" "filepathtomajors" "assignmentcolumntitle" lowerthresholdinclusive higherthresholdinclusive
@@ -24,7 +24,7 @@ def main():
 	with open(args.gradebook, newline='', encoding='utf-8') as f:
 		reader = csv.DictReader(f)
 		for row in reader:
-			grades[row[args.grade_id]] = row[args.grade_column]
+			grades[row[args.grade_id]] = float(row[args.grade_column])
 			
 	with open(args.majorlist, newline='', encoding='utf-8') as f:
 		reader = csv.DictReader(f)
@@ -40,6 +40,14 @@ def main():
 			continue
 		else:
 			print(id + " " + majors[id] + " " + str(grades[id]))
+			
+	met_threshold = {major: [0, 0] for major in majors.values()}
+	for id in grades:
+		if id in majors:
+			met_threshold[majors[id]][1] += 1
+			met_threshold[majors[id]][0] += 1 if grades[id] >= args.lower_threshold and grades[id] < args.higher_threshold else 0
+	print(met_threshold)
+	
 
 if __name__ == "__main__":
 	main()
