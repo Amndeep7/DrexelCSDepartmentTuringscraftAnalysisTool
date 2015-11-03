836c219b1e624480990d9a997e6d29ee63d2890c
Amndeep Singh Mann <amndeep.dev@gmail.com>
2015-11-03 16:32:21 -0500

    Added verbosity check
    Also refactored slightly, as well as added comments

--- range.py
+++ range.py
@@ -1,6 +1,7 @@
 import argparse
 import csv
 
+# usage: python range.py "filepathtogradebook" "filepathtomajors" "filepathtoresults" "gradebook_studentid" "gradebook_assignment" "major_studentid" "major_name" lowerthresholdinclusive higherthresholdexclusive [--verbose]
 def get_arguments():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('gradebook')
@@ -12,42 +13,51 @@ def get_arguments():
 	parser.add_argument('major_column')
 	parser.add_argument('lower_threshold', type=float)
 	parser.add_argument('higher_threshold', type=float)
+	parser.add_argument('-v', '--verbose', action='store_true')
 	return parser.parse_args()
 
-# usage: python range.py "filepathtogradebook" "filepathtomajors" "filepathtoresults" "gradebook_studentid" "gradebook_assignment" "major_studentid" "major_name" lowerthresholdinclusive higherthresholdexclusive
+def read_in_data(source, id, data, repository, wrapper=lambda a: a):
+	with open(source, newline='', encoding='utf-8') as f:
+		reader = csv.DictReader(f)
+		for row in reader:
+			repository[row[id]] = wrapper(row[data])
+
 def main():
 	args = get_arguments()
+	
+	if args.verbose:
+		print(args)
 
 	grades = dict()
 	majors = dict()
 	
-	with open(args.gradebook, newline='', encoding='utf-8') as f:
-		reader = csv.DictReader(f)
-		for row in reader:
-			grades[row[args.grade_id]] = float(row[args.grade_column])
+	# read in data from gradebook and majorlist
+	read_in_data(args.gradebook, args.grade_id, args.grade_column, grades, float)
+	read_in_data(args.majorlist, args.major_id, args.major_column, majors)
+
+	# preliminary warning
+	if args.verbose and len(grades) != len(majors):
+		print('There is a size mismatch between the gradebook and the majors list; however, analysis will do as much as it can.')
+	
+	# sanity check
+	if args.verbose:
+		for id in grades:
+			if id not in majors:
+				print('There is a student in the gradebook who is not on the majors list with the id: ' + id + '.')
+				continue
+			else:
+				print(id + " " + majors[id] + " " + str(grades[id]))
 			
-	with open(args.majorlist, newline='', encoding='utf-8') as f:
-		reader = csv.DictReader(f)
-		for row in reader:
-			majors[row[args.major_id]] = row[args.major_column]
-			
-	if len(grades) != len(majors):
-		print('There is a size mismatch between the gradebook and the majors list.')
-		
-	for id in grades:
-		if id not in majors:
-			print('There is a student in the gradebook who is not on the majors list.')
-			continue
-		else:
-			print(id + " " + majors[id] + " " + str(grades[id]))
-			
-	met_threshold = {major: [0, 0] for major in majors.values()}
+	# analysis
+	met_threshold = {major: [0, 0] for major in majors.values()} # [number_met_threshold, total_number]
 	for id in grades:
 		if id in majors:
 			met_threshold[majors[id]][1] += 1
 			met_threshold[majors[id]][0] += 1 if grades[id] >= args.lower_threshold and grades[id] < args.higher_threshold else 0
-	print(met_threshold)
+	if args.verbose:
+		print(met_threshold)
 	
+	# print to csv
 	with open(args.range_results, 'w', newline='', encoding='utf-8') as f:
 		headers = ['Major', 'Total Students', 'Met Threshold', 'Percentage']
 		writer = csv.DictWriter(f, fieldnames=headers)
@@ -55,8 +65,9 @@ def main():
 		writer.writeheader()
 		for major in met_threshold:
 			writer.writerow({headers[0]: major, headers[1]: met_threshold[major][1], headers[2]: met_threshold[major][0], headers[3]: "{0:f}".format(round(100*met_threshold[major][0]/met_threshold[major][1]))})
-			
-	print('done')	
+	
+	if args.verbose:
+		print('done')	
 
 if __name__ == "__main__":
 	main()
