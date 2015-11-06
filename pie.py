c4f4b7520dbd715619645ddc9a99239384b83828
Mansoor Siddiqui <mis43@drexel.edu>
2015-11-06 12:25:40 -0500

    updated piechart with comments

--- pie.py
+++ pie.py
@@ -1,21 +1,29 @@
 from pylab import *
 
+# Define dictionaries for total students and students that met the threshold within each major
 totalStudents = {'BACS': 50, 'BSCS': 38, 'BASE': 5, 'BSSE': 10}
 metStudents = {'BACS': 48, 'BSCS': 33, 'BASE': 3, 'BSSE': 8}
 
+# Function that creates all pie charts
 def createPieCharts(grade, total, met, minThreshold, maxThreshold):
+    # Run through each major in the dictionary
     for major, total in total.items():
-        # make a square figure and axes
+        # Make a square figure and axes
         figure(1, figsize=(6,6))
         ax = axes([0.1, 0.1, 0.8, 0.8])
         # The slices will be ordered and plotted counter-clockwise.
+        # Set labels, and calculate percentage values
         labels = 'Met threshold', 'Did not meet threshold'
         fracs = [met[major]/total, (total-met[major])/total]
+        # Pop the slice of the pie out for emphasis
         explode=(0.1, 0)
+        # Create the chart
         pie(fracs, explode=explode, labels=labels,
                autopct='%1.1f%%', shadow=True, startangle=90)
+        # Set the title
         title('%s Students Between %s-%s for Assignment %s' % (major, minThreshold, maxThreshold, grade),
                  bbox={'facecolor':'0.8', 'pad':5})
+        # Save the figure as a png
         savefig("piechart_%s_%s_%s-%s.png" % (grade, major, minThreshold, maxThreshold), bbox_inches="tight")
         clf()
 
