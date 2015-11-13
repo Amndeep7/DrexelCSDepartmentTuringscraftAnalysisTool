9b852e46ca6037f4132fa95466346baf72dde048
Mansoor Siddiqui <mis43@drexel.edu>
2015-11-13 13:50:26 -0500

    Made most of amndeep's code review changes. code is in production mode

--- pie.py
+++ pie.py
@@ -1,35 +1,49 @@
 from matplotlib.pylab import figure, axes, pie, title, savefig, clf
+import csv
+import sys
 
-# Define dictionaries for total students and students that met the threshold within each major
-totalStudents = {'BACS': 50, 'BSCS': 38, 'BASE': 5, 'BSSE': 10}
-metStudents = {'BACS': 48, 'BSCS': 33, 'BASE': 3, 'BSSE': 8}
+#To run from command line
+#   python pie.py CSV_FileName Name_Of_Grade Minimum_Threshold Maximum_Threshold
 
-# Function that creates all pie charts
-def createPieCharts(grade, total, met, minThreshold, maxThreshold):
-    # Run through each major in the dictionary
+# Read CSV to define dictionaries for total students and students that met the threshold within each major
+#    totalStudents = {'BACS': 50, 'BSCS': 38, 'BASE': 5, 'BSSE': 10}
+#    metStudents = {'BACS': 48, 'BSCS': 33, 'BASE': 3, 'BSSE': 8}
+def readCSV(filename):
+    total_stu = dict()
+    met_stu = dict()
+    with open(filename, newline='', encoding='utf-8') as csvfile:
+        inputcsv = csv.DictReader(csvfile)
+        for row in inputcsv:
+            total_stu[row['Major']] = int(row['Total Students'])
+            met_stu[row['Major']] = int(row['Met Threshold'])
+    return total_stu, met_stu
+
+def createPieCharts(grade, total, met, min_threshold, max_threshold):
     for major, total in total.items():
         # Make a square figure and axes
         figure(1, figsize=(6,6))
         ax = axes([0.1, 0.1, 0.8, 0.8])
         # The slices will be ordered and plotted counter-clockwise.
-        # Set labels, and calculate percentage values
         labels = 'Met threshold', 'Did not meet threshold'
         fracs = [met[major]/total, (total-met[major])/total]
         # Pop the slice of the pie out for emphasis
         explode=(0.1, 0)
-        # Create the chart
         pie(fracs, explode=explode, labels=labels,
                autopct='%1.1f%%', shadow=True, startangle=90)
-        # Set the title
-        title('%s Students Between %s-%s for Assignment %s' % (major, minThreshold, maxThreshold, grade),
+        title('%s Students Between %s-%s for Assignment %s' % (major, min_threshold, max_threshold, grade),
                  bbox={'facecolor':'0.8', 'pad':5})
         # Save the figure as a png
-        savefig("piechart_%s_%s_%s-%s.png" % (grade, major, minThreshold, maxThreshold), bbox_inches="tight")
+        savefig("piechart_%s_%s_%s-%s.png" % (grade, major, min_threshold, max_threshold), bbox_inches="tight")
         clf()
 
 def main():
-    args = read_in_args()
-    # createPieCharts("A2", totalStudents, metStudents, 80, 100)
+    #System arguments should be as follows
+    #   sys.argv[1] = Filename of CSV generated from range.py
+    #   sys.argv[2] = Name of Grade
+    #   sys.argv[3] = Minimum Threshold value
+    #   sys.argv[4] = Maximum Threshold value
+    totalStudents, metStudents = readCSV(sys.argv[1])
+    createPieCharts(sys.argv[2], totalStudents, metStudents, sys.argv[3], sys.argv[4])
 
-if name == "main":
+if __name__ == "__main__":
     main()
