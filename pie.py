97acf78c44ffd510bca7da166ea785ef3183aab0
Amndeep Singh Mann <amndeep.dev@gmail.com>
2015-11-20 13:51:53 -0500

    Renamed variable names

--- pie.py
+++ pie.py
@@ -6,26 +6,24 @@ import sys
 #   python pie.py CSV_FileName Name_Of_Grade Minimum_Threshold Maximum_Threshold
 
 # Read CSV to define dictionaries for total students and students that met the threshold within each major
-#    totalStudents = {'BACS': 50, 'BSCS': 38, 'BASE': 5, 'BSSE': 10}
-#    metStudents = {'BACS': 48, 'BSCS': 33, 'BASE': 3, 'BSSE': 8}
-def readCSV(filename):
-    total_stu = dict()
-    met_stu = dict()
+def read_csv(filename):
+    total_students = dict()
+    met_students = dict()
     with open(filename, newline='', encoding='utf-8') as csvfile:
         inputcsv = csv.DictReader(csvfile)
         for row in inputcsv:
-            total_stu[row['Major']] = int(row['Total Students'])
-            met_stu[row['Major']] = int(row['Met Threshold'])
-    return total_stu, met_stu
+            total_students[row['Major']] = int(row['Total Students'])
+            met_students[row['Major']] = int(row['Met Threshold'])
+    return total_students, met_students
 
-def createPieCharts(grade, total, met, min_threshold, max_threshold):
-    for major, total in total.items():
+def create_piecharts(grade, total_students, met_students, min_threshold, max_threshold):
+    for major, total in total_students.items():
         # Make a square figure and axes
         figure(1, figsize=(6,6))
         ax = axes([0.1, 0.1, 0.8, 0.8])
         # The slices will be ordered and plotted counter-clockwise.
         labels = 'Met threshold', 'Did not meet threshold'
-        fracs = [met[major]/total, (total-met[major])/total]
+        fracs = [met_students[major]/total, (total-met_students[major])/total]
         # Pop the slice of the pie out for emphasis
         explode=(0.1, 0)
         pie(fracs, explode=explode, labels=labels,
@@ -42,8 +40,8 @@ def main():
     #   sys.argv[2] = Name of Grade
     #   sys.argv[3] = Minimum Threshold value
     #   sys.argv[4] = Maximum Threshold value
-    totalStudents, metStudents = readCSV(sys.argv[1])
-    createPieCharts(sys.argv[2], totalStudents, metStudents, sys.argv[3], sys.argv[4])
+    total_students, met_students = read_csv(sys.argv[1])
+    create_piecharts(sys.argv[2], total_students, met_students, sys.argv[3], sys.argv[4])
 
 if __name__ == "__main__":
     main()
