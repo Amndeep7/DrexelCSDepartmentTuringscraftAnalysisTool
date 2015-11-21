7e6945c39585c139d78ce2b8c588de3ef73dc2f6
Amndeep Singh Mann <amndeep.dev@gmail.com>
2015-11-21 18:23:53 -0500

    Minor clean-up

--- sample_data.py
+++ sample_data.py
@@ -1,24 +1,23 @@
-from random import randint
-import random
-import csv #importing the CSV module
+from random import choice, randint
+import csv
 
-def sampleData():
-	idCreator = [] 
-	majorCreator = ["BACS", "BSCS", "BASE", "BSSE", "XYZ"]
+def generate_sample_data():
+	id_creator = [] 
+	major_creator = ["BACS", "BSCS", "BASE", "BSSE", "XYZ"]
 	c = csv.writer(open("major.csv", "wb")) #create an object for writing
-	c.writerow(["Student ID","Major"]) #write the CSV file
+	c.writerow(["Student ID", "Major"]) #write the CSV file
 	c1 = csv.writer(open("gradebook.csv", "wb"))
 	c1.writerow(["Student ID", "A1 Grade", "A2 Grade", "A3 Grade"])
 
 	for x in range(50):
 		for i in range(0,7):
-			idCreator.append(randint(0,9))
-		studentId = ''.join(str(e) for e in idCreator)
+			id_creator.append(randint(0,9))
+		student_id = ''.join(str(e) for e in id_creator)
 		
-		major = random.choice(majorCreator)
-		grade1 = random.randint(50, 100)
-		grade2 = random.randint(50, 100)
-		grade3 = random.randint(50, 100)
-		idCreator = []
-		c.writerow([studentId,major])
-		c1.writerow([studentId,grade1,grade2,grade3])
+		major = choice(major_creator)
+		grade1 = randint(50, 100)
+		grade2 = randint(50, 100)
+		grade3 = randint(50, 100)
+		id_creator = []
+		c.writerow([student_id, major])
+		c1.writerow([student_id, grade1, grade2, grade3])
