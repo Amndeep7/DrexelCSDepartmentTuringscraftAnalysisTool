be570567829a93ae983d14a1041cfddf8e518e81
Amndeep Singh Mann <amndeep.dev@gmail.com>
2015-11-21 20:52:57 -0500

    ﻿Complete rewrite.
    Rewrote sample data generation function, added calling as script functionality (which means that it takes arguments now as well).
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    # On branch development
    # Your branch is ahead of 'origin/development' by 4 commits.
    #   (use "git push" to publish your local commits)
    #
    # Changes to be committed:
    #	modified:   sample_data.py
    #

--- sample_data.py
+++ sample_data.py
@@ -1,23 +1,48 @@
-from random import choice, randint
+import argparse
 import csv
+import random
+import string
 
-def generate_sample_data():
-	id_creator = [] 
-	major_creator = ["BACS", "BSCS", "BASE", "BSSE", "XYZ"]
-	c = csv.writer(open("major.csv", "w")) #create an object for writing
-	c.writerow(["Student ID", "Major"]) #write the CSV file
-	c1 = csv.writer(open("gradebook.csv", "w"))
-	c1.writerow(["Student ID", "A1 Grade", "A2 Grade", "A3 Grade"])
+def get_arguments():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('gradebook')
+	parser.add_argument('majorlist')
+	parser.add_argument('sample_size', type=int)
+	parser.add_argument('num_majors', type=int)
+	parser.add_argument('num_grades', type=int)
+	return parser.parse_args()
 
-	for x in range(50):
-		for i in range(0,7):
-			id_creator.append(randint(0,9))
-		student_id = ''.join(str(e) for e in id_creator)
+def generate_sample_data(sample_size, num_majors, num_grades, gradebook, majorlist):
+	ids = [random.randrange(10000000, 100000000) for x in range(sample_size)] # student ids are 8 digits long
+	major_names = [''.join(random.choice(string.ascii_uppercase) for x in range(random.randrange(3, 7))) for y in range(num_majors)] # major ids are generally between 3 and 6 characters long
+	grade_names = [''.join(random.choice(string.ascii_uppercase) for x in range(3)) + str(y+1) for y in range(num_grades)]
+	
+	majors = {id: random.choice(major_names) for id in ids}
+	grades = {id: {name: min(max(0, round(random.gauss(70, 15), 2)), 100) for name in grade_names} for id in ids}
+	
+	with open(majorlist, 'w', newline='', encoding='utf-8') as f:
+		headers = ['Student ID', 'Major']
+		writer = csv.DictWriter(f, fieldnames=headers)
+		writer.writeheader()
 		
-		major = choice(major_creator)
-		grade1 = randint(50, 100)
-		grade2 = randint(50, 100)
-		grade3 = randint(50, 100)
-		id_creator = []
-		c.writerow([student_id, major])
-		c1.writerow([student_id, grade1, grade2, grade3])
+		id_keys = ids
+		random.shuffle(id_keys)
+		for id_key in id_keys:
+			writer.writerow({headers[0]: id_key, headers[1]: majors[id_key]})
+			
+	with open(gradebook, 'w', newline='', encoding='utf-8') as f:
+		headers = ['Student ID'] + grade_names
+		writer = csv.DictWriter(f, fieldnames=headers)
+		writer.writeheader()
+		
+		id_keys = ids
+		random.shuffle(id_keys)
+		for id_key in id_keys:
+			writer.writerow({**{headers[0]: id_key}, **{grade_name: grades[id_key][grade_name] for grade_name in grade_names}}) # c = {**a, **b} where a, b are dicts results in a dict merging the two (introduced in 3.5)
+			
+def main():
+	args = get_arguments()
+	generate_sample_data(args.sample_size, args.num_majors, args.num_grades, args.gradebook, args.majorlist)
+	
+if __name__ == "__main__":
+	main()
