5ba1e42d9cdbd5d586a2bf1dea9b1a28f69e7fdf
Amndeep Singh Mann <amndeep.dev@gmail.com>
2016-01-27 18:45:22 -0500

    Turingscraft problems checker 'completed'

--- check.py
+++ check.py
@@ -1,73 +1,75 @@
 import argparse
 import csv
 
-# usage: python range.py "filepathtogradebook" "filepathtomajors" "filepathtoresults" "gradebook_studentid" "gradebook_assignment" "major_studentid" "major_name" lowerthresholdinclusive higherthresholdexclusive [--verbose]
+# usage: python check.py "filepathtobblearnroster" "filepathtoturingscraftdata" "filepathtoproblemslist" "filepathtoresults" "roster_studentid" "turingscraft_studentid" "problems_id" [-s "sectionid" sectionnumber]
 def get_arguments():
 	parser = argparse.ArgumentParser()
-	parser.add_argument('gradebook')
-	parser.add_argument('majorlist')
-	parser.add_argument('range_results')
-	parser.add_argument('grade_id')
-	parser.add_argument('grade_column')
-	parser.add_argument('major_id')
-	parser.add_argument('major_column')
-	parser.add_argument('lower_threshold', type=float)
-	parser.add_argument('higher_threshold', type=float)
-	parser.add_argument('-v', '--verbose', action='store_true')
+	parser.add_argument('bblearn')
+	parser.add_argument('turingscraft')
+	parser.add_argument('problems')
+	parser.add_argument('check_results')
+	parser.add_argument('bblearn_id')
+	parser.add_argument('turingscraft_id')
+	parser.add_argument('problems_id')
+	parser.add_argument('-s', '--section', nargs=2)
 	return parser.parse_args()
 
-def read_in_data(source, id, data, repository, wrapper=lambda a: a):
+# conditional = [id, value] or None
+def read_in_data_to_list(source, id, repository, conditional=None):
 	with open(source, newline='', encoding='utf-8') as f:
 		reader = csv.DictReader(f)
 		for row in reader:
-			repository[row[id]] = wrapper(row[data])
+			if conditional:
+				if row[conditional[0]] == conditional[1]:
+					repository.append(row[id])
+			else:
+				repository.append(row[id])
+
+def read_in_data_to_dictionary(source, id, data, repository):
+	with open(source, newline='', encoding='utf-8') as f:
+		reader = csv.DictReader(f)
+		for row in reader:
+			repository[row[id]] = list()
+			for datum in data:
+				repository[row[id]].append(row[datum])
+
+def identify_headers(source, partials):
+	with open(source, newline='', encoding='utf-8') as f:
+		line = f.readline().split(',')
+		conversion = list()
+		for header in line:
+			convert = header.split('-')
+			if len(convert) == 2:
+				convert = convert[1]
+			else:
+				convert = convert[0]
+			if convert in partials:
+				conversion.append(header)
+		return conversion
 
 def main():
 	args = get_arguments()
-	
-	if args.verbose:
-		print(args)
 
-	grades = dict()
-	majors = dict()
+	names = list()
+	problems = list()
+	checks = dict()
 	
-	# read in data from gradebook and majorlist
-	read_in_data(args.gradebook, args.grade_id, args.grade_column, grades, float)
-	read_in_data(args.majorlist, args.major_id, args.major_column, majors)
-
-	# preliminary warning
-	if args.verbose and len(grades) != len(majors):
-		print('There is a size mismatch between the gradebook and the majors list; however, analysis will do as much as it can.')
-	
-	# sanity check
-	if args.verbose:
-		for id in grades:
-			if id not in majors:
-				print('There is a student in the gradebook who is not on the majors list with the id: ' + id + '.')
-				continue
-			else:
-				print(id + " " + majors[id] + " " + str(grades[id]))
-			
-	# analysis
-	met_threshold = {major: [0, 0] for major in majors.values()} # [number_met_threshold, total_number]
-	for id in grades:
-		if id in majors:
-			met_threshold[majors[id]][1] += 1
-			met_threshold[majors[id]][0] += 1 if grades[id] >= args.lower_threshold and grades[id] < args.higher_threshold else 0
-	if args.verbose:
-		print(met_threshold)
+	# read in data from the roster, the list of problems, and the turingscraft data
+	read_in_data_to_list(args.bblearn, args.bblearn_id, names, args.section)
+	names = list(map(lambda n: n+"@drexel.edu", names))
+	read_in_data_to_list(args.problems, args.problems_id, problems)
+	read_in_data_to_dictionary(args.turingscraft, args.turingscraft_id, identify_headers(args.turingscraft, problems), checks)
 	
 	# print to csv
-	with open(args.range_results, 'w', newline='', encoding='utf-8') as f:
-		headers = ['Major', 'Total Students', 'Met Threshold', 'Percentage']
+	with open(args.check_results, 'w', newline='', encoding='utf-8') as f:
+		headers = ['Email'] + problems + ['Total']
 		writer = csv.DictWriter(f, fieldnames=headers)
 		
 		writer.writeheader()
-		for major in met_threshold:
-			writer.writerow({headers[0]: major, headers[1]: met_threshold[major][1], headers[2]: met_threshold[major][0], headers[3]: "{0:f}".format(round(100*met_threshold[major][0]/met_threshold[major][1]))})
-	
-	if args.verbose:
-		print('done')	
+		for name in names:
+			total = checks[name].count('c')
+			values = [name] + checks[name] + [total]
+			writer.writerow(dict(zip(headers, values)))
 
 if __name__ == "__main__":
 	main()
